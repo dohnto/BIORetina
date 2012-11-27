d3e9f6c3f32399264e7e145f2f5ed675854673d4
Tomas Dohnalek <dohnto@gmail.com>
2012-11-27 18:29:38 +0100

    BIORetina project created.

---
+++ BIO.Framework/BIO.Project.BIORetina/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BIO.Project.BIORetina
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //this object has responsibility for creating all needed objects
+            ProjectSettings settings = new ProjectSettings();
+        }
+    }
+}
---
+++ BIO.Framework/BIO.Project.BIORetina/ProjectSettings.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BIO.Framework.Extensions.Standard.Database.InputDatabase;
+using BIO.Framework.Extensions.Emgu.InputData;
+
+namespace BIO.Project.BIORetina
+{
+    class ProjectSettings :
+        BIO.Project.ProjectSettings<StandardRecord<StandardRecordData>, EmguGrayImageInputData>,
+        IStandardProjectSettings<StandardRecord<StandardRecordData>>
+    {
+        #region IStandardProjectSettings<StandardRecord<StandardRecordData>> Members
+        public int TemplateSamples { get { return 1;} }
+        #endregion
+
+        public override Framework.Core.Database.IDatabaseCreator<StandardRecord<StandardRecordData>> getDatabaseCreator()
+        {
+            return new RetinaDatabaseCreator(@"C:\Users\tom\Skola\bio\BIORetina\datasets\VARIA");
+
+        }
+
+        protected override Framework.Core.Evaluation.Block.IBlockEvaluatorSettings<StandardRecord<StandardRecordData>, EmguGrayImageInputData> getEvaluatorSettings()
+        {
+            return new RetinaEvaluationSettings();
+        }
+
+    }
+}
